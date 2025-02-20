77d6e3d7a0d5280e37cb056cd5e3c3900880f5f4
0xW3bJun6l3 <luca@HackBook.local>
2025-02-20 09:34:34 +0100

    Migration to Integration

--- install.sh
+++ install.sh
@@ -29,55 +29,16 @@ SETUP_DIR="ogbinst"
 CMDLINE_FILE="/boot/firmware/cmdline.txt"
 PARAMS="apparmor=1 security=apparmor"
 
-
-# RUCKECYAML
-RUCKEHACCONF="https://raw.githubusercontent.com/OpenGrow-Box/PWM-Fans/main/RuckEC_webapi/airCtrlSensor.yaml"
-RUCKCONNF="https://raw.githubusercontent.com/OpenGrow-Box/PWM-Fans/main/RuckEC_webapi/configuration.yaml"
-
 # Sources 
 OGB_SOURCE="https://github.com/OpenGrow-Box/OpenGrowBox.git"
 RUCK_SOURCE="https://github.com/OpenGrow-Box/PWM-Fans"
 OS_AGENT_SOURCE="https://github.com/home-assistant/os-agent/releases/download/1.6.0/os-agent_1.6.0_linux_aarch64.deb"
 HA_SOURCE="https://github.com/home-assistant/supervised-installer/releases/latest/download/homeassistant-supervised.deb"
 
-
-# SETUP STUFF
-ONBOARDING="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/onboarding"
-ROOMS="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/core.area_registr.json"
-
-MAINCONF="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/configuration.yaml"
-ADDONS="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/addons.json"
-NODEREDCONF="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/n-r-options.json"
-EDITORCONF="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/editor-options.json"
-
-ALLFLOWS="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/NodeRed/Core/All/All-Flows.json"
-
-## UI 
-OPG_DASHBOARD="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/lovelace_dashboards.json"
-DASHBOARDVIEW="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/lovelace.dashboard_opengrowview.json"
-
-
 ## ADDONS 
-OGB="https://github.com/OpenGrow-Box/OpenGrowBox-HA/archive/refs/tags/v1.1.3.zip"
-OGB_EXHAUST="https://github.com/OpenGrow-Box/OpenGrowBox-Exhaust/archive/refs/tags/main.zip"
+OGB="https://github.com/OpenGrow-Box/OpenGrowBox-HA/archive/refs/tags/v1.2.0._1.zip"
+OGB_EXHAUST="https://github.com/OpenGrow-Box/OpenGrowBox-Exhaust/archive/refs/tags/0.2.zip"
 
-NRCOMPANION="https://github.com/zachowj/hass-node-red/archive/refs/heads/main.zip"
-MINIGRAPH="https://github.com/kalkih/mini-graph-card/releases/download/v0.12.1/mini-graph-card-bundle.js"
-
-
-MUSHROOM="https://github.com/piitaya/lovelace-mushroom/releases/download/v3.6.2/mushroom.js"
-LLRESSOURCE="https://raw.githubusercontent.com/OpenGrow-Box/OpenGrowBox/main/UI/setupFiles/lovelace_resources.json"
-
-VPDCHART="https://raw.githubusercontent.com/OpenGrow-Box/vpdchart-card/master/vpdchart-card.js"
-
-# New
-VERTICALCARD="https://github.com/ofekashery/vertical-stack-in-card/releases/download/v1.0.1/vertical-stack-in-card.js"
-CUSTOMTEMPPLATE="https://github.com/iantrich/config-template-card/releases/download/1.3.6/config-template-card.js"
-TAPPEDCARD="https://github.com/kinghat/tabbed-card/releases/download/v0.3.2/tabbed-card.js"
-AUTOENTIES="https://github.com/thomasloven/lovelace-auto-entities/archive/refs/tags/v1.13.0.zip"
-BUBBLECARD="https://raw.githubusercontent.com/Clooos/Bubble-Card/main/dist/bubble-card.js"
-BUBBLEFIX="https://raw.githubusercontent.com/Clooos/Bubble-Card/main/dist/bubble-pop-up-fix.js"
-LAYOUTCARD="https://github.com/thomasloven/lovelace-layout-card/archive/refs/tags/v2.4.5.zip"
 
 function systemPrep() {
     sudo apt update && sudo apt upgrade -y && sudo apt autoclean -y && sudo apt autoremove -y
@@ -104,8 +65,6 @@ function ruckapiSetup() {
 
     sudo cp bin/* /opt/ruckecapi/.
     sudo cp ruckecapi.service /etc/systemd/system/.
-
-    sudo systemctl enable --now ruckecapi.service
     
 }
 
@@ -137,13 +96,12 @@ function homeassistantSetup() {
 function HACSsetup(){
     wget -O - https://get.hacs.xyz > /tmp/hacs_inst.sh
     sudo bash /tmp/hacs_inst.sh
-
 }
 
 function postSetup(){
     echo "Install ADDONS"
     ha addons install core_configurator
-    ha addons install a0d7b954_nodered
+
 }
 
 function OpenGrowBoxSetup() {
@@ -152,30 +110,8 @@ function OpenGrowBoxSetup() {
     #sudo wget -O /usr/share/hassio/homeassistant/.storage/onboarding "$ONBOARDING"
 
     # Startup Addons
-    ha addons start a0d7b954_nodered
-    ha addons start core_configurator
+
     sleep 5
-    sudo wget -O /usr/share/hassio/addon_configs/a0d7b954_nodered/flows.json "$ALLFLOWS"
-    sudo wget -O /usr/share/hassio/addons.json "$ADDONS"
-    sudo wget -O /usr/share/hassio/addons/data/a0d7b954_nodered/options.json "$NODEREDCONF" 
-    sudo wget -O /usr/share/hassio/addons/data/core_configurator/options.json "$EDITORCONF"
-
-
-
-    wget  -O lovelace_resources "$LLRESSOURCE" 
-
-    sudo wget -O  /usr/share/hassio/homeassistant/.storage/lovelace_dashboards "$OPG_DASHBOARD" 
-    sudo wget -O  /usr/share/hassio/homeassistant/.storage/lovelace.dashboard_opengrowbox "$DASHBOARDVIEW"
-
-    # Node-Red-Companion-Setup
-    echo "Setup Node-Red-Companion-Integration"
-    cd /usr/share/hassio/homeassistant/custom_components
-    sudo mkdir nodered 
-    cd nodered
-    sudo wget -O nodered.zip "$NRCOMPANION"
-    sudo unzip nodered.zip
-    sudo mv hass-node-red-main/custom_components/nodered/* .
-    sudo rm -rf hass-node-red-main nodered.zip
 
      # OGB_HA SETUP
     echo "Instllation OGB HA-Integrations"
@@ -194,39 +130,6 @@ function OpenGrowBoxSetup() {
     sudo unzip ogbexhaust.zip
     sudo mv OpenGrowBox-Exhaust-main/custom_components/ogb_exhaust/* .
     sudo rm -rf OpenGrowBox-Exhaust-main ogbexhaust.zip
-
-    # Setup mini-Graph
-    sudo mkdir -p /usr/share/hassio/homeassistant/www/
-    cd /usr/share/hassio/homeassistant/www/
-    sudo wget $MINIGRAPH
-    # Setup VPD-Chart
-    sudo wget -O "vpdchart-card.js"  "$VPDCHART" 
-
-    # Setup Mushroom
-    sudo wget $MUSHROOM
-    
-    # Setup Custom Template Card
-    sudo wget  -O "config-template-card.js" "$CUSTOMTEMPPLATE"
-    
-    # Setup Vertical Stack Card
-    sudo wget -O "vertical-stack-in-card.js"  "$VERTICALCARD" 
-    
-    # Setup Tabbed Card
-    sudo wget  -O "tabbed-card.js" "$TAPPEDCARD"
-
-    # Setup Auto Entities
-    sudo wget  -O "auto-entities.zip" "$AUTOENTIES" && sudo unzip auto-entities.zip && sudo rm auto-entities.zip  
-    # Setup LayoutCard
-    sudo wget  -O "layout.zip"  "$LAYOUTCARD" && sudo unzip layout.zip && sudo rm layout.zip
-    # Setup Bubble Cardhist 
-    sudo wget -O "bubble-card.js" "$BUBBLECARD" 
-    sudo wget  -O "bubble-pop-up-fix.js" "$BUBBLEFIX"
-
-    # SETUP loveLace_Ressource
-    cd /usr/share/hassio/homeassistant/.storage/ && sudo wget  -O lovelace_resources "$LLRESSOURCE"
-
-    # Setup Predefined Rooms and Integrations 
-    #sudo wget -O /usr/share/hassio/homeassistant/.storage/core.area_registr "$ROOMS"
 
 }
 
